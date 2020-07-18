5f106a02f97c797b9bab5b2b40f0150f25608bf3
aybjax <aybat93@gmail.com>
2020-07-18 09:57:16 +0600

    Add files via upload

--- corewar1/printInitLexerLines.c
+++ corewar1/printInitLexerLines.c
@@ -1,5 +1,7 @@
 #include "myHeader.h"
 
+void printTypes(int type);
+
 int printInitLexerLines(InitLines* initLines)
 {
     InitLines* lines = initLines;
@@ -27,7 +29,7 @@ int printInitLexerLines(InitLines* initLines)
                 else printf("%-2s --/ ", " ");
                 while(lexer != NULL)
                 {
-                    printf("%-10d", lexer->type);
+                    printTypes(lexer->type);
                     if(lexer->next != NULL) printf(" %s ", "\\--/");
                     lexer = lexer->next;
                 }
@@ -84,15 +86,53 @@ int printInitLexerLines(InitLines* initLines)
     printf("\n");
 
     printf("types are as follows:\n");
-    printf("\t%15d - %c\n", QUOTE_TYPE, QUOTE_CHAR);
-    printf("\t%15d - %c\n", COMMENT_TYPE, COMMENT_CHAR);
-    printf("\t%15d - %c\n", LABEL_TYPE, LABEL_CHAR);
-    printf("\t%15d - %c\n", DIRECT_TYPE, DIRECT_CHAR);
-    printf("\t%15d - %c\n", SEPARATOR_TYPE, SEPARATOR_CHAR);
-    printf("\t%15d - %s\n", NAME_CMD_TYPE, NAME_CMD_STRING);
-    printf("\t%15d - %s\n", COMMENT_CMD_TYPE, COMMENT_CMD_STRING);
-    printf("\t%15d - %s\n", LABEL_STRING_TYPE, LABEL_CHARS);
-    printf("\t%15d - %s\n", NUMBER_TYPE, NUMBER_CHARS);
-    printf("\t%15d - %cNBR\n", REG_TYPE,REG_CHAR);
-    printf("\t%15d - %s\n", UNKNOWN_TYPE, "UNKNOWN TYPE");
+    printf("\t%15d - %-40c - ", QUOTE_TYPE, QUOTE_CHAR);
+    printTypes(QUOTE_TYPE);
+    printf("\n");
+    printf("\t%15d - %-40c - ", COMMENT_TYPE, COMMENT_CHAR);
+    printTypes(COMMENT_TYPE);
+    printf("\n");
+    printf("\t%15d - %-40c - ", LABEL_TYPE, LABEL_CHAR);
+    printTypes(LABEL_TYPE);
+    printf("\n");
+    printf("\t%15d - %-40c - ", DIRECT_TYPE, DIRECT_CHAR);
+    printTypes(DIRECT_TYPE);
+    printf("\n");
+    printf("\t%15d - %-40c - ", SEPARATOR_TYPE, SEPARATOR_CHAR);
+    printTypes(SEPARATOR_TYPE);
+    printf("\n");
+    printf("\t%15d - %-40s - ", NAME_CMD_TYPE, NAME_CMD_STRING);
+    printTypes(NAME_CMD_TYPE);
+    printf("\n");
+    printf("\t%15d - %-40s - ", COMMENT_CMD_TYPE, COMMENT_CMD_STRING);
+    printTypes(COMMENT_CMD_TYPE);
+    printf("\n");
+    printf("\t%15d - %-40s - ", LABEL_STRING_TYPE, LABEL_CHARS);
+    printTypes(LABEL_STRING_TYPE);
+    printf("\n");
+    printf("\t%15d - %-40s - ", NUMBER_TYPE, NUMBER_CHARS);
+    printTypes(NUMBER_TYPE);
+    printf("\n");
+    printf("\t%15d - %cNBR%-36s - ", REG_TYPE,REG_CHAR, " ");
+    printTypes(REG_TYPE);
+    printf("\n");
+    printf("\t%15d - %-40s - ", UNKNOWN_TYPE, "UNKNOWN TYPE");
+    printTypes(UNKNOWN_TYPE);
+    printf("\n");
+}
+
+
+void printTypes(int type)
+{
+    if( type == QUOTE_TYPE ) printf("%-10s", "quote");
+    if( type == COMMENT_TYPE ) printf("%-10s", "comm char");
+    if( type == LABEL_TYPE ) printf("%-10s", "label char");
+    if( type == DIRECT_TYPE ) printf("%-10s", "dir char");
+    if( type == SEPARATOR_TYPE ) printf("%-10s", "separator");
+    if( type == NAME_CMD_TYPE ) printf("%-10s", "prog name");
+    if( type == COMMENT_CMD_TYPE ) printf("%-10s", "prog comm");
+    if( type == LABEL_STRING_TYPE ) printf("%-10s", "any str");
+    if( type == NUMBER_TYPE ) printf("%-10s", "nbr");
+    if( type == REG_TYPE ) printf("%-10s", "registry");
+    if( type == UNKNOWN_TYPE ) printf("%-10s", "unknown");
 }
